e201c11a19030e6909df8429d9915f8ab29e15ee
Trisotto <longaraitrisotto.flt@gmail.com>
2024-06-24 01:44:03 -0300

    EP - Update Login Endpoint Response

--- src/main/java/com/trainsys/trainsys_application/controller/AuthenticationController.java
+++ src/main/java/com/trainsys/trainsys_application/controller/AuthenticationController.java
@@ -9,10 +9,12 @@ import com.trainsys.trainsys_application.service.AuthenticationService;
 import com.trainsys.trainsys_application.service.JwtService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RequestMapping("/auth")
 @RestController
@@ -38,18 +40,24 @@ public class AuthenticationController {
             signUpResponse.setRoleDescription(registeredUser.getRole().getDescription());
             return new ResponseEntity<>(signUpResponse, HttpStatus.CREATED);
         } catch (Exception  e) {
-            return new ResponseEntity<>("Invalid request: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(STR."Invalid request: \{e.getMessage()}", HttpStatus.BAD_REQUEST);
         }
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
-        UserEntity authenticatedUser = authenticationService.authenticate(loginUserDto);
+    public ResponseEntity<?> authenticate(@RequestBody LoginUserDto loginUserDto) {
+        try{
+            UserEntity authenticatedUser = authenticationService.authenticate(loginUserDto);
 
-        String jwtToken = jwtService.generateToken(authenticatedUser);
+            String jwtToken = jwtService.generateToken(authenticatedUser);
 
-        LoginResponse loginResponse = new LoginResponse().setToken(jwtToken).setExpiresIn(jwtService.getExpirationTime());
+            LoginResponse loginResponse = new LoginResponse().setToken(jwtToken).setExpiresIn(jwtService.getExpirationTime()).setName(authenticatedUser.getName());
 
-        return ResponseEntity.ok(loginResponse);
+            return ResponseEntity.ok(loginResponse);
+        } catch (BadCredentialsException e) {
+            return new ResponseEntity<>(STR."Invalid request: \{e.getMessage()}", HttpStatus.UNAUTHORIZED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(STR."Invalid request: \{e.getMessage()}", HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- src/main/java/com/trainsys/trainsys_application/response/LoginResponse.java
+++ src/main/java/com/trainsys/trainsys_application/response/LoginResponse.java
@@ -1,9 +1,13 @@
 package com.trainsys.trainsys_application.response;
 
+import java.util.concurrent.TimeUnit;
+
 public class LoginResponse {
     private String token;
 
-    private long expiresIn;
+    private Long expiresIn;
+
+    private String name;
 
     public String getToken() {
         return token;
@@ -14,17 +18,27 @@ public class LoginResponse {
         return this;
     }
 
-    public long getExpiresIn() {
+    public Long getExpiresIn() {
         return expiresIn;
     }
 
-    public LoginResponse setExpiresIn(long expiresIn) {
-        this.expiresIn = expiresIn;
+    public LoginResponse setExpiresIn(Long expiresIn) {
+        this.expiresIn = TimeUnit.MILLISECONDS.toHours(expiresIn);
         return this;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public LoginResponse setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+
     @Override
     public String toString() {
-        return STR."LoginResponse{token='\{token}\{'\''}, expiresIn=\{expiresIn}\{'}'}";
+        return STR."LoginResponse{token='\{token}\{'\''}, expiresIn=\{expiresIn}, name='\{name}\{'\''}\{'}'}";
     }
 }
